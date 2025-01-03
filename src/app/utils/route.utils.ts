e8826d02121beffb382eab4e54eb6b4332e87974
Anass Lahbabi <anass-lahbabi@hotmail.com>
2025-01-03 11:09:28 +0100

    Navigation works

--- src/app/utils/route.utils.ts
+++ src/app/utils/route.utils.ts
@@ -5,14 +5,25 @@ import { environment } from '../../environments/environment.prod';
 import { getRoutesByHost } from './host.utils';
 import { getRoutesByPort } from './port.utils';
 
-const hostService = new HostService();
+// const hostService = new HostService();
+
+// export const getRoutes = (): Routes => {
+//   if (environment.production) {
+//     const host = hostService.getCurrentHost();
+//     return getRoutesByHost(host);
+//   } else {
+//     const port = window.location.port;
+//     return getRoutesByPort(port);
+//   }
+// };
 
 export const getRoutes = (): Routes => {
   if (environment.production) {
-    const host = hostService.getCurrentHost();
+    const host = window.location.hostname.split('.')[0];
     return getRoutesByHost(host);
   } else {
-    const port = window.location.port;
+    const port = window.location.port || '4200';
+    console.log('Getting routes for port:', port);
     return getRoutesByPort(port);
   }
 };
